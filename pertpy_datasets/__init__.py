b9c50825ef9074b4375d061161a615cb59b3d939
zethson <lukas.heumos@posteo.net>
2024-11-11 17:03:54 +0100

    Add package
    Signed-off-by: zethson <lukas.heumos@posteo.net>

---
+++ pertpy_datasets/__init__.py
@@ -0,0 +1,7 @@
+__author__ = "Lukas Heumos"
+__email__ = "lukas.heumos@posteo.net"
+__version__ = "0.1.0"
+
+from pertpy_datasets.perturbation_curator import PerturbationCurator
+
+__all__ = ["PerturbationCurator"]
---
+++ pertpy_datasets/perturbation_curator.py
@@ -0,0 +1,104 @@
+from typing import TYPE_CHECKING, Literal
+
+import anndata as ad
+
+if TYPE_CHECKING:
+    from lnschema_core import Record
+
+
+class _PerturbationValidatorUnavailable:
+    """Curator for perturbation data."""
+
+    def __init__(self):
+        raise RuntimeError("PerturbationValidator can only be instantiated if connected to a lamindb instance.")
+
+
+# Nested try because django might not be installed
+try:
+    from django.core.exceptions import ImproperlyConfigured
+
+    try:
+        import bionty as bt
+        import wetlab as wl
+        from cellxgene_lamin import CellxGeneFields, Curate
+        from lamin_utils import logger
+        from lamindb_setup.core.types import UPathStr
+        from lnschema_core.types import FieldAttr
+
+        class PerturbationCurator(Curate):
+            """Curator flow for Perturbation data - see pertpy-datasets."""
+
+            def __init__(
+                self,
+                adata: ad.AnnData | UPathStr,
+                var_index: FieldAttr = bt.Gene.ensembl_gene_id,
+                organism: Literal["human", "mouse"] = "human",
+                *,
+                verbosity: str = "hint",
+                cxg_schema_version: Literal["5.0.0", "5.1.0"] = "5.1.0",
+                using_key: str = "laminlabs/pertpy-datasets",
+            ):
+                """Curator flow for Perturbation data.
+
+                Args:
+                    adata: Path to or AnnData object to curate against the CELLxGENE schema.
+                    var_index: The registry field for mapping the ``.var`` index.
+                    categoricals: A dictionary mapping ``.obs.columns`` to a registry field.
+                        The PerturbationCurator maps against the required CELLxGENE fields and perturbation fields by default.
+                    organism: The organism name. CELLxGENE restricts it to 'human' and 'mouse' and therefore so do we.
+                    defaults: Default values that are set if columns or column values are missing.
+                    extra_sources: A dictionary mapping ``.obs.columns`` to Source records.
+                    verbosity: The verbosity level.
+                    cxg_schema_version: The CELLxGENE schema version to curate against.
+                    using_key: A reference LaminDB instance.
+                """
+                PT_DEFAULT_VALUES = CellxGeneFields.OBS_FIELD_DEFAULTS | {
+                    "cell_line": "unknown",
+                    "genetic_treatments": "",
+                    "compound_treatments": "",
+                    "environmental_treatments": "",
+                    "combination_treatments": "",
+                }
+
+                PT_CATEGORICALS = CellxGeneFields.OBS_FIELDS | {
+                    "cell_line": bt.CellLine.name,
+                    "genetic_treatments": wl.GeneticTreatment.name,
+                    "compound_treatments": wl.CompoundTreatment.name,
+                    "environmental_treatments": wl.EnvironmentalTreatment.name,
+                    "combination_treatments": wl.CombinationTreatment.name,
+                }
+
+                PT_SOURCES: dict[str, Record] = {
+                    "depmap_id": bt.Source.filter(name="depmap").one(),
+                    "cell_line": bt.Source.filter(name="depmap").one(),
+                    # "compound_treatments": bt.Source.filter(entity="Drug", name="chebi").first()
+                }
+
+                self.organism = organism
+
+                # Set the Compound source to chebi; we don't want output if the source has already been set
+                with logger.mute():
+                    chebi_source = bt.Source.filter(entity="Drug", name="chebi").first()
+                    wl.Compound.add_source(chebi_source)
+
+                super().__init__(
+                    adata=adata,
+                    var_index=var_index,
+                    categoricals=PT_CATEGORICALS,
+                    using_key=using_key,
+                    defaults=PT_DEFAULT_VALUES,
+                    verbosity=verbosity,
+                    organism=self.organism,
+                    extra_sources=PT_SOURCES,
+                    schema_version=cxg_schema_version,
+                )
+
+            def validate(self) -> bool:
+                """Validates the AnnData object against cellxgene and pertpy's requirements."""
+                return super().validate()
+
+    except ImproperlyConfigured:
+        PerturbationCurator = _PerturbationValidatorUnavailable  # type: ignore
+
+except ImportError:
+    PerturbationCurator = _PerturbationValidatorUnavailable  # type: ignore
